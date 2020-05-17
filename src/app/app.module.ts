f3c858eee4137f9f8cbfe35d85c7d59eabf6a3a7
Dimitar Goshevski <roundrobine@gmail.com>
2020-05-17 20:07:59 +0200

    passing event from parent to child event component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { EventListComponent } from "./events/events-list.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
@@ -6,7 +7,11 @@ import { EventsAppComponent } from "./events-app.component";
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [EventsAppComponent, EventListComponent],
+  declarations: [
+    EventsAppComponent,
+    EventListComponent,
+    EventThumbnailComponent,
+  ],
   bootstrap: [EventsAppComponent],
 })
 export class AppModule {}

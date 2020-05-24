7cc5b37647a4caa5566816b4a1a90327649b3b8b
Dimitar Goshevski <roundrobine@gmail.com>
2020-05-24 15:09:48 +0200

    add events list component and render it with ngFor and ngSwitch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,18 +1,38 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "event-thumbnail",
   template: ` <div class="well hoverwell thumbnail">
     <h2>{{ event.name }}</h2>
     <div>Date: {{ event.date }}</div>
-    <div>Time: {{ event.time }}</div>
-    <div>Price: \${{ event.price }}</div>
-    <div>
-      <span>Location: {{ event.location.address }}</span>
-      <span>&nbsp;</span>
-      <span>{{ event.location.city }}, {{ event.location.country }}</span>
+    <div [ngSwitch]="event?.time">
+      Time: {{ event.time }}
+      <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+      <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+      <span *ngSwitchDefault>(Normal Start)</span>
     </div>
+    <div>Price: \${{ event.price }}</div>
+    <div *ngIf="event?.location">
+      <span>Location: {{ event.location?.address }}</span>
+      <span class="pad-left"
+        >{{ event.location?.city }}, {{ event.location?.country }}</span
+      >
+    </div>
+    <div *ngIf="event?.onlineUrl">Online Url: {{ event.onlineUrl }}</div>
   </div>`,
+  styles: [
+    `
+      .thumbnail {
+        min-height: 210px;
+      }
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `,
+  ],
 })
 export class EventThumbnailComponent {
   @Input() event: any;

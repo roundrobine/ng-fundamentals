90b89ffdc3ed7b7ed16140901845011f253205a8
Dimitar Goshevski <roundrobine@gmail.com>
2020-05-25 16:28:05 +0200

    create a simple login component with userName and password fields only

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,11 @@ import { RouterModule } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ProfileComponent } from "./profile/profile.component";
+import { LoginComponent } from "./login/login.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  declarations: [ProfileComponent],
-  imports: [CommonModule, RouterModule.forChild(userRoutes)],
+  declarations: [ProfileComponent, LoginComponent],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(userRoutes)],
 })
 export class UserModule {}
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,8 @@
+import { LoginComponent } from "./login/login.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { Routes } from "@angular/router";
 
 export const userRoutes: Routes = [
   { path: "profile", component: ProfileComponent },
+  { path: "login", component: LoginComponent },
 ];

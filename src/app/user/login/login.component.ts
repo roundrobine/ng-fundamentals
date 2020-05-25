37fb90f7e763c6b6548f648d4f860eb1927c9e14
Dimitar Goshevski <roundrobine@gmail.com>
2020-05-25 17:19:18 +0200

    add required filed validation on userName and password fields

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -10,6 +10,7 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
   userName;
   password;
+  mouseoverLogin;
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
